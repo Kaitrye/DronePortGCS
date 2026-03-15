3e94a7c692a539ae46f1b997f9d97b88b40caf24
Kaitrye <bogdanova.tekna156@yandex.ru>
2026-03-15 21:42:43 +0300

    Remove draft drone_port

--- systems/drone_port/tests/test_dummy_unit.py
+++
@@ -1,5 +0,0 @@
-import pytest
-from unittest.mock import MagicMock
-
-from systems.drone_port.src.dp_comp_a.src.dp_comp import DummyComponent
-from systems.drone_port.src.dp_comp_b.src.dp_comp import DummyComponent as DummyComponentB
