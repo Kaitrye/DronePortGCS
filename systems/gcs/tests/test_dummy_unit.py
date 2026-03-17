ddc3790b964de55bac78ca950660877407bc67be
Kaitrye <bogdanova.tekna156@yandex.ru>
2026-03-17 14:26:56 +0300

    Fix main changes

--- systems/gcs/tests/test_dummy_unit.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from unittest.mock import MagicMock
-
-from systems.gcs.src.gcs_comp_a.src.gcs_comp import DummyComponent
-from systems.gcs.src.gcs_comp_b.src.gcs_comp import DummyComponent as DummyComponentB
-
